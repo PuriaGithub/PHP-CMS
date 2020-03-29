1ff50f537573fdc19df99ac5332fcdf2746ea330
Puria Golgoonnejad <puriagit@gmail.com>
2020-03-29 13:14:59 +1100

    make page assets reusable + page title

--- private/shared/dashboard/header.php
+++ private/shared/dashboard/header.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Basic CMS with PHP</title>
+    <title>Basic CMS with PHP | <?php echo isset($page_title) ? $page_title : 'Page Title'; ?></title>
 </head>
 
 <body>
