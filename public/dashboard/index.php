1be7f21d306f12b3aed5aec1c71c667c97c10558
Puria Golgoonnejad <puriagit@gmail.com>
2020-03-29 12:16:46 +1100

    initial commit

---
+++ public/dashboard/index.php
@@ -0,0 +1,9 @@
+<?php require_once('../../private/initialize.php') ?>
+
+<?php include('../../private/shared/dashboard/header.php') ?>
+
+<div class="content">
+    <h1>Welcome to Dashboard</h1>
+</div>
+
+<?php include('../../private/shared/dashboard/footer.php') ?>
---
+++ public/index.php
@@ -0,0 +1,2 @@
+<?php
+    echo 'Basic CMS - PHP';
